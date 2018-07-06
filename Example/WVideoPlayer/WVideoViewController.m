292fb1b4e3e137dc810d21d68e63c6b8cbdc3d62
zzttwzq <1076976262@qq.com>
2018-07-06 09:29:46 +0800

    Initial commit

---
+++ Example/WVideoPlayer/WVideoViewController.m
@@ -0,0 +1,29 @@
+//
+//  WVideoViewController.m
+//  WVideoPlayer
+//
+//  Created by zzttwzq on 07/06/2018.
+//  Copyright (c) 2018 zzttwzq. All rights reserved.
+//
+
+#import "WVideoViewController.h"
+
+@interface WVideoViewController ()
+
+@end
+
+@implementation WVideoViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Example/WVideoPlayer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WVideoPlayer
+//
+//  Created by zzttwzq on 07/06/2018.
+//  Copyright (c) 2018 zzttwzq. All rights reserved.
+//
+
+@import UIKit;
+#import "WVideoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WVideoAppDelegate class]));
+    }
+}
